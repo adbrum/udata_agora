808eee2d79b8141a4a336b4cd2ea28fc172b2b1a
ines <ines.correia@babelgroup.com>
2026-02-18 17:42:34 +0000

    feat: display CardNoResults component when no datasets are found.

--- src/components/datasets/DatasetsClient.tsx
+++ src/components/datasets/DatasetsClient.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Button, InputSearchBar, Icon, CardGeneral, InputSelect, DropdownSection, DropdownOption, Pill } from '@ama-pt/agora-design-system';
+import { Button, InputSearchBar, Icon, CardGeneral, InputSelect, DropdownSection, DropdownOption, Pill, CardNoResults } from '@ama-pt/agora-design-system';
 import { Pagination } from '@/components/Pagination';
 import { DatasetsFilters } from '@/components/datasets/DatasetsFilters';
 import { APIResponse, Dataset } from '@/types/api';
@@ -98,87 +98,104 @@ export default function DatasetsClient({
                 </div>
 
                 <div className="flex flex-col">
-                  {datasets.map((dataset, index) => (
-                    <div
-                      key={dataset.id}
-                      className={`border-t md:pt-24 border-[#E1E4EA]  ${index === datasets.length - 1
-                        ? 'border-b'
-                        : ''
-                        }`}
-                    >
-                      <div className="dataset-card-modern">
-                        <CardGeneral
-                          isCardHorizontal={true}
-                          variant="white"
-                          image={{
-                            src: dataset.organization?.logo || '/images/placeholders/organization.png',
-                            alt: dataset.organization?.name || 'Organização sem logo',
-                          }}
-                          subtitleText={dataset.organization?.name || 'Organização Desconhecida'}
-                          titleText={
-                            (
-                              <Link href={`/pages/datasets/${dataset.slug}`} className="hover:underline text-primary-900 font-bold text-lg">
-                                {dataset.title}
-                              </Link>
-                            ) as unknown as string
-                          }
-                          descriptionText={
-                            (
-                              <div className="flex flex-col gap-8">
-                                <span className="text-xs ">
-                                  Atualizado há{' '}
-                                  {formatDistanceToNow(new Date(dataset.last_modified), {
-                                    locale: pt,
-                                  })}
-                                </span>
+                  {datasets.length > 0 ? (
+                    datasets.map((dataset, index) => (
+                      <div
+                        key={dataset.id}
+                        className={`border-t md:pt-24 border-[#E1E4EA]  ${index === datasets.length - 1
+                          ? 'border-b'
+                          : ''
+                          }`}
+                      >
+                        <div className="dataset-card-modern">
+                          <CardGeneral
+                            isCardHorizontal={true}
+                            variant="white"
+                            image={{
+                              src: dataset.organization?.logo || '/images/placeholders/organization.png',
+                              alt: dataset.organization?.name || 'Organização sem logo',
+                            }}
+                            subtitleText={dataset.organization?.name || 'Organização Desconhecida'}
+                            titleText={
+                              (
+                                <Link href={`/pages/datasets/${dataset.slug}`} className="hover:underline text-primary-900 font-bold text-lg">
+                                  {dataset.title}
+                                </Link>
+                              ) as unknown as string
+                            }
+                            descriptionText={
+                              (
+                                <div className="flex flex-col gap-8">
+                                  <span className="text-xs ">
+                                    Atualizado há{' '}
+                                    {formatDistanceToNow(new Date(dataset.last_modified), {
+                                      locale: pt,
+                                    })}
+                                  </span>
 
-                                <p className=" text-sm line-clamp-2 leading-relaxed">
-                                  {dataset.description}
-                                </p>
+                                  <p className=" text-sm line-clamp-2 leading-relaxed">
+                                    {dataset.description}
+                                  </p>
 
-                                <Pill appearance="solid" variant="warning" className="w-fit h-fit inline-flex items-center">
-                                  Metadados: 35%
-                                </Pill>
+                                  <Pill appearance="solid" variant="warning" className="w-fit h-fit inline-flex items-center">
+                                    Metadados: 35%
+                                  </Pill>
 
-                                <div className="flex items-center gap-16 text-sm  mt-8">
-                                  <div className="flex items-center gap-8" title="Visualizações">
-                                    <Icon name="agora-line-eye" className="w-20 h-20" aria-hidden="true" />
-                                    <span className="font-medium">
-                                      {dataset.metrics?.views
-                                        ? dataset.metrics.views >= 1000000
-                                          ? (dataset.metrics.views / 1000000).toFixed(1).replace('.', ',') + ' M'
-                                          : dataset.metrics.views >= 1000
-                                            ? (dataset.metrics.views / 1000).toFixed(0) + ' mil'
-                                            : dataset.metrics.views
-                                        : '0'}
-                                    </span>
-                                  </div>
-                                  <div className="flex items-center gap-8" title="Downloads">
-                                    <Icon name="agora-line-download" className="w-20 h-20" aria-hidden="true" />
-                                    <span className="font-medium">
-                                      {dataset.metrics?.downloads
-                                        ? dataset.metrics.downloads >= 1000
-                                          ? (dataset.metrics.downloads / 1000).toFixed(0) + ' mil'
-                                          : dataset.metrics.downloads
-                                        : '0'}
-                                    </span>
-                                  </div>
-                                  <div className="flex items-center gap-8" title="Reutilizações">
-                                    <Icon name="agora-line-chart-bar" className="w-20 h-20" aria-hidden="true" />
-                                    <span className="font-medium">{dataset.metrics?.reuses || 0}</span>
-                                  </div>
-                                  <div className="flex items-center gap-8" title="Seguidores">
-                                    <Icon name="agora-line-star" className="w-20 h-20" aria-hidden="true" />
-                                    <span className="font-medium">{dataset.metrics?.followers || 0}</span>
+                                  <div className="flex items-center gap-16 text-sm  mt-8">
+                                    <div className="flex items-center gap-8" title="Visualizações">
+                                      <Icon name="agora-line-eye" className="w-20 h-20" aria-hidden="true" />
+                                      <span className="font-medium">
+                                        {dataset.metrics?.views
+                                          ? dataset.metrics.views >= 1000000
+                                            ? (dataset.metrics.views / 1000000).toFixed(1).replace('.', ',') + ' M'
+                                            : dataset.metrics.views >= 1000
+                                              ? (dataset.metrics.views / 1000).toFixed(0) + ' mil'
+                                              : dataset.metrics.views
+                                          : '0'}
+                                      </span>
+                                    </div>
+                                    <div className="flex items-center gap-8" title="Downloads">
+                                      <Icon name="agora-line-download" className="w-20 h-20" aria-hidden="true" />
+                                      <span className="font-medium">
+                                        {dataset.metrics?.downloads
+                                          ? dataset.metrics.downloads >= 1000
+                                            ? (dataset.metrics.downloads / 1000).toFixed(0) + ' mil'
+                                            : dataset.metrics.downloads
+                                          : '0'}
+                                      </span>
+                                    </div>
+                                    <div className="flex items-center gap-8" title="Reutilizações">
+                                      <Icon name="agora-line-chart-bar" className="w-20 h-20" aria-hidden="true" />
+                                      <span className="font-medium">{dataset.metrics?.reuses || 0}</span>
+                                    </div>
+                                    <div className="flex items-center gap-8" title="Seguidores">
+                                      <Icon name="agora-line-star" className="w-20 h-20" aria-hidden="true" />
+                                      <span className="font-medium">{dataset.metrics?.followers || 0}</span>
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                            ) as unknown as string
-                          }
-                        />
+                              ) as unknown as string
+                            }
+                          />
+                        </div>
                       </div>
+                    ))
+                  ) : (
+                    <div className="mt-32">
+                      <CardNoResults
+                        icon={<Icon name="agora-line-search" className="w-12 h-12 text-primary-500" />}
+                        title="Não encontrou o que procurava?"
+                        subtitle={<span className="font-bold">Tente redefinir os filtros para ampliar sua busca.</span>}
+                        description="Você também pode visualizar as solicitações atuais e enviar as suas próprias em nosso fórum dedicado à pesquisa de dados e ao acesso aberto."
+                        position="center"
+                        hasAnchor={true}
+                        valueAnchor="Redefir filtros"
+                        anchorHref="/pages/datasets"
+                        anchorTrailingIcon="agora-line-arrow-right-circle"
+                        anchorTrailingIconHover="agora-solid-arrow-right-circle"
+                      />
                     </div>
-                  ))}
+                  )}
                 </div>
 
                 <div className="mt-64 flex justify-center pb-64">
