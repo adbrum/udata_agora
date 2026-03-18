6ea9d55e9cc202742b9b400d42e994ea123740f9
João Conceição <jms.conceicao98@gmail.com>
2026-03-18 10:51:06 +0000

    updates on layout content pages

--- src/components/articles/AboutOpenData.tsx
+++ src/components/articles/AboutOpenData.tsx
@@ -6,8 +6,6 @@ const AboutOpenData = () => {
   return (
     <GitHubArticlePage
       slug="pages/faqs/about_opendata"
-      title="Sobre dados abertos"
-      publishedDate="18.12.2025"
       breadcrumbItems={[
         { label: "Início", url: "/" },
         { label: "Conhecimento", url: "#" },
